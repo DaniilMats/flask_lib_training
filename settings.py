0082bf50514ad846d7fd3f82e49d2930cf0d888f
Daniil <d.matsuytsya@admitad.com>
2020-04-26 16:45:07 +0300

    added annotations

--- settings.py
+++ settings.py
@@ -2,11 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
 
-dir_path = os.path.dirname(os.path.realpath(__file__))+'/database.db'
-app = Flask(__name__)
+dir_path: str = os.path.dirname(os.path.realpath(__file__))+'/database.db'
+app: Flask = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dir_path
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+db: SQLAlchemy = SQLAlchemy(app)
 db.create_all()
 

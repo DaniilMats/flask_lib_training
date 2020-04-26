7e779c2841a4065be789a5f07782359ee6f1fb91
Daniil <d.matsuytsya@admitad.com>
2020-04-26 14:09:39 +0300

    added delete and update options

--- BookModel.py
+++ BookModel.py
@@ -12,16 +12,36 @@ class Book(db.Model):
     book_name = db.Column(db.String(80), nullable=False)
     id = db.Column(db.Integer, primary_key=True)
 
-    def add_book(_author, _book_name, _id):
-        new_book = Book(author=_author, book_name=_book_name, id=_id)
+    def add_book(book_info: dict):
+        new_book = Book(author=book_info.get('author'),
+                        book_name=book_info.get('book_name'),
+                        id=book_info.get('id'))
         db.session.add(new_book)
         db.session.commit()
 
+    def json(self):
+        return {'author': self.author, 'book_name': self.book_name, 'id': self.id}
+
     def get_all_books():
-        return Book.query.all()
+        return [Book.json(book) for book in Book.query.all()]
 
     def get_book_by_id(_id):
         return Book.query.filter_by(id=_id).first()
+
+    def delete_book(_id):
+        Book.query.filter_by(id=_id).delete()
+        db.session.commit()
+
+    def update_book(_id: int, book_info: dict):
+        book_to_update = Book.query.filter_by(id=_id).first()
+        author_name: str = book_info.get('author')
+        book_name: str = book_info.get('book_name')
+        if author_name:
+            book_to_update.author = author_name
+        if book_name:
+            book_to_update.book_name = book_name
+        db.session.commit()
+
 
     def __repr__(self):
         book: dict = {

c423bb92cd83a3867a75231aa9f6dc450e79e975
Daniil <d.matsuytsya@admitad.com>
2020-04-26 12:35:33 +0300

    changed db name

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:/home/dan/PycharmProjects/flask_edu/database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/dan/PycharmProjects/flask_edu/database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False

181c9c5cc8200685e806a0eb9774379cff446141
Daniil <d.matsuytsya@admitad.com>
2020-04-26 14:09:56 +0300

    added path detection of db

--- settings.py
+++ settings.py
@@ -1,5 +1,7 @@
 from flask import Flask
+import os
 
+dir_path = os.path.dirname(os.path.realpath(__file__))+'/database.db'
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/dan/PycharmProjects/flask_edu/database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dir_path
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
